515a12d125cf36ac6e3316dfdc6ae3c1b8e43be9
brunomontoya17 <brucemontosh17@gmail.com>
2024-03-20 11:44:58 -0300

    Correccion en Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -8,10 +8,10 @@ function Item({item}) {
   const options = { style: 'currency', currency: 'ARS' };
   return (
     <Card style={{ width : '200px' , float:'left'}}>
-        <CardBody as={Link} to={`/item/${item.id}`} >
+        <CardBody>
             <CardTitle>{item.name}</CardTitle>
             <CardSubtitle><b>Precio: </b>{new Intl.NumberFormat('es-AR',options).format(item.price)}</CardSubtitle>
-            <CardImg src={item.img} />
+            <Link to={`/item/${item.id}`}><CardImg src={item.img} /></Link>
         </CardBody>
     </Card>
   )
